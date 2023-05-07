f1cb11391d80e808bfa3c78c36101503f45e28e6
flaviumircia <flaviumircia15@gmail.com>
2023-05-07 13:15:56 +0300

    Update: Added some documentation for Configurations.java and ReadCredentials.java

--- src/main/java/com/flaviumircia/backendlab4/configs/Configurations.java
+++ src/main/java/com/flaviumircia/backendlab4/configs/Configurations.java
@@ -12,13 +12,19 @@ import java.util.Map;
 
 @Configuration
 public class Configurations {
+    /**
 
+     Returns a configured DataSource object using the credentials from a text file.
+     The text file should be located in the project's source folder and contain the
+     database username on the first line and password on the second line.
+     @return a configured DataSource object
+     @throws FileNotFoundException if the file containing the credentials cannot be found
+     */
     @Bean
     public DataSource dataSource() throws FileNotFoundException {
         Map<String,String> credentials;
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
         credentials=ReadCredentials.getCredentialsFromRelative("/src/main/java/com/flaviumircia/backendlab4/misc/Credentials.txt");
-
         dataSource.setUsername(credentials.get("username"));
         dataSource.setPassword(credentials.get("password"));
         dataSource.setUrl(
--- src/main/java/com/flaviumircia/backendlab4/misc/ReadCredentials.java
+++ src/main/java/com/flaviumircia/backendlab4/misc/ReadCredentials.java
@@ -7,7 +7,14 @@ import java.util.Map;
 import java.util.Scanner;
 
 public class ReadCredentials {
+    /**
 
+     Reads the credentials from a file at a relative path to the current directory and returns them in a Map.
+     The file should contain two lines, the first being the username and the second being the password.
+     @param path the relative path to the file containing the credentials
+     @return a Map containing the username and password as key-value pairs
+     @throws FileNotFoundException if the file cannot be found
+     */
     public static Map<String,String> getCredentialsFromRelative(String path) throws FileNotFoundException {
         String localDir = System.getProperty("user.dir");
         File file = new File(localDir+path);
